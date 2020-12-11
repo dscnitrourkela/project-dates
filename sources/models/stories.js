d5470d6882f1852e0d4a82e76d98cd6db873da01
HarishTeens <arishh2@gmail.com>
2020-12-11 13:06:15 +0530

    set eol prettier confi to auto

--- sources/models/stories.js
+++ sources/models/stories.js
@@ -1,12 +1,14 @@
+/** @format */
+
 var mongoose = require('mongoose');
 var eventSchema = new mongoose.Schema({
-  storyId: Number,
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Club',
-  },
-  posted_at: String,  
-  picture: String,
+	storyId: Number,
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Club',
+	},
+	posted_at: String,
+	picture: String,
 });
 
 module.exports = mongoose.model('Event', eventSchema);
