1f3433906209432ce7ec4ba2f3a4f0b7f6ad9e07
HarishTeens <arishh2@gmail.com>
2021-01-28 21:07:54 +0530

    clean up
    - remove unused dependecies
    - edit package.json
    - gitignore package-lock

--- sources/index.js
+++ sources/index.js
@@ -22,14 +22,6 @@ mongoose.connection.once('open', () => {
 	// seed.seedPermissions();
 });
 
-//Cloudinary Config
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
-});
-
-
 server.listen(5000).then(({ url }) => {
 	console.log(`Graphql running on ${url}`);
 });
