598efd50d6847a8b3c8bc7e32c930ef0424ea923
roshankshaw <roshankushaw1@gmail.com>
2021-01-29 11:09:37 +0530

    Combine resolvers component files in  apollo directory

--- sources/apollo/resolvers.js
+++ sources/apollo/resolvers.js
@@ -1,9 +1,23 @@
 /** @format */
 
-const Query = require('./aggregateQuery');
-const Mutation = require('./aggregateMutation');
-const FieldResolver = require('./aggregateFieldResolver');
+const User = require('./users/user.resolver.js');
+const Event = require('./events/event.resolver.js');
+const Club = require('./clubs/club.resolver.js');
+const Venue = require('./venues/venue.resolver.js');
+const AccessLevel = require('./accessLevels/accessLevel.resolver.js');
+const Story = require('./stories/story.resolver.js');
 const { GraphQLDateTime } =require ("graphql-iso-date");
+
+const FieldResolver = {};
+const Query = {};
+const Mutation = {};
+
+const schemas = [User, Event, Club, AccessLevel,Story];
+schemas.forEach((s) => {
+	Object.assign(FieldResolver, s.fieldResolvers);
+	Object.assign(Query, s.queries);
+	Object.assign(Mutation, s.mutations);
+});
 
 //Custom Resolver for Date object
 const CustomScalarResolver = { 
