00a9c0c8508ffb42cd2516dba69de7899e3f8fa0
DesignrKnight <notify.abelmathew@gmail.com>
2021-02-03 15:27:19 +0530

    Setup PM2 config and monitoring

--- sources/ecosystem.config.js
+++ sources/ecosystem.config.js
@@ -1,27 +1,36 @@
-module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.',
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    },
-    exec_mode  : "cluster",
-    instances: -1
+/** @format */
 
-  },],
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+module.exports = {
+	apps: [
+		{
+			name: 'dev',
+			script: 'index.js',
+			watch: '.',
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+			exec_mode: 'cluster',
+			instances: -1,
+		},
+		{
+			name: 'prod',
+			script: 'index.js',
+			watch: false,
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+			exec_mode: 'cluster',
+			instances: 0,
+			error_file: './logs/err.log',
+			out_file: './logs/out.log',
+			log_file: './logs/combined.log',
+			time: true,
+		},
+	],
 };
