6a04a3b6b436359456fbc17890dd3a3acec44645
Abhibhaw Asthana <39991296+abhibhaw@users.noreply.github.com>
2022-08-01 09:11:08 +0530

    feat: Upgrades packages to latest v (#100)

--- sources/index.js
+++ sources/index.js
@@ -1,30 +1,26 @@
-
+/** @format */
 
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const server = require("./apollo/server");
-const PORT_NUMBER = 5000;
+const server = require('./apollo/server');
+const PORT_NUMBER = 8000;
 dotenv.config();
 
-//Mongoose Configs
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/elaichi', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/elaichi');
 mongoose.connection.once('open', () => {
-	console.info('connected to the database')
-	console.timeEnd("mongo")
+	console.info('connected to the database');
+	console.timeEnd('mongo');
 	// seed.seedData();
 	// seed.seedPermissions();
 });
+mongoose.connection.on('error', err => {
+	console.error(err);
+	console.timeEnd('mongo');
+});
 
 server.listen(PORT_NUMBER).then(({ url }) => {
-	console.info(`Graphql running on ${url}`);	
-	console.timeEnd("apollo")
+	console.info(`Graphql running on ${url}`);
+	console.timeEnd('apollo');
 });
-console.time("apollo")
-console.time("mongo")
+console.time('apollo');
+console.time('mongo');
