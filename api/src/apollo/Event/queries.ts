2da3f4b6adae1cb0dcb3b189f4a6851f6384b24e
Ritesh Patil <riteshsp2000@gmail.com>
2022-10-18 20:35:30 +0530

    Feat/event model (#112)
    * feat: create event prisma and nexus type
    
    * feat: create event queries and mutations

---
+++ api/src/apollo/Event/queries.ts
@@ -0,0 +1,52 @@
+import { idArg, list, nonNull, queryField } from 'nexus';
+
+export const getEvent = queryField('getEvent', {
+  type: 'Event',
+  args: {
+    id: nonNull(idArg()),
+  },
+  resolve(_parent, args, { prisma }) {
+    return prisma.event.findUnique({
+      where: {
+        id: args.id,
+      },
+    });
+  },
+});
+
+export const getEvents = queryField('getEvents', {
+  type: list('Event'),
+  args: {
+    orgID: idArg(),
+    orgType: 'orgType',
+    startDate: 'DateTime',
+    endDate: 'DateTime',
+    status: 'status',
+  },
+  resolve(_parent, args, { prisma }) {
+    /**
+     * based on orgType
+     * based on orgID
+     * based on time (start and end)
+     * based on status
+     */
+
+    const prismaQuery = {
+      orgType: args.orgType || undefined,
+      status: args.status || undefined,
+      orgID: { has: args.orgID || undefined },
+      AND: [
+        { startDate: { gte: args.startDate || undefined } },
+        { endDate: { lte: args.endDate || undefined } },
+      ],
+    };
+
+    if (args) {
+      return prisma.event.findMany({
+        where: prismaQuery,
+      });
+    }
+
+    throw new Error('missing parameters');
+  },
+});
---
+++ api/src/apollo/Event/type.ts
@@ -0,0 +1,33 @@
+import { objectType } from 'nexus';
+
+export const Event = objectType({
+  name: 'Event',
+  description:
+    'Refers to the various events created by the different organisations',
+  definition(t) {
+    t.nonNull.id('id');
+    t.nonNull.string('name');
+    t.nonNull.string('description');
+    t.nonNull.string('poster');
+    t.nonNull.id('location');
+    t.nonNull.date('startDate');
+    t.nonNull.date('endDate');
+    t.nonNull.list.nonNull.id('orgID');
+    t.nonNull.orgType('orgType');
+    t.nonNull.list.nonNull.string('notes');
+    t.nonNull.list.nonNull.id('poc');
+    t.nonNull.boolean('weekly');
+    t.repeatType('repeatDay');
+    t.nonNull.int('priority');
+    t.nonNull.status('status');
+  },
+});
+
+/**
+ * getEventById
+ * getEventsByOrgId
+ * getEventsByOrgType (mess, etc)
+ * getEventsByPoc
+ * getEventsOfTheDay
+ * getEventsByStatus
+ */
