345ac2b3bcdb63c54db3dbf40e3444a55828740f
Ritesh Patil <riteshsp2000@gmail.com>
2022-10-22 17:03:15 +0530

    fix: add field resolver for festID in team type

--- api/src/apollo/Team/type.ts
+++ api/src/apollo/Team/type.ts
@@ -7,8 +7,34 @@ export const Team = objectType({
     t.nonNull.id('id');
     t.string('position');
     t.string('team');
-    t.nonNull.id('userID');
-    t.nonNull.id('orgID');
     t.int('priority');
+
+    t.nonNull.id('userID');
+    t.nonNull.field('user', {
+      type: 'User',
+      async resolve(parent, _args, { prisma }) {
+        const user = await prisma.user.findUnique({
+          where: { id: parent.userID },
+        });
+
+        if (!user) throw new Error('User not found');
+        return user;
+      },
+    });
+
+    t.nonNull.id('orgID');
+    t.nonNull.field('org', {
+      type: 'Org',
+      async resolve(parent, _arg, { prisma }) {
+        const org = await prisma.org.findUnique({
+          where: {
+            id: parent.orgID,
+          },
+        });
+
+        if (!org) throw new Error('Org not found');
+        return org;
+      },
+    });
   },
 });
