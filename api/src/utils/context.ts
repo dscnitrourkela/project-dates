d6fec6ce44eae5334b95d5d45b046a4710ef00d5
abhibhaw <asthana3110@gmail.com>
2022-10-27 15:01:32 +0530

    feat: Authenticates a sample query

--- api/src/utils/context.ts
+++ api/src/utils/context.ts
@@ -1,11 +1,15 @@
+import { Request } from 'express';
+
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
 
 export interface Context {
   prisma: PrismaClient;
+  req: Request;
 }
 
-export const context = async (): Promise<Context> => ({
+export const context = async ({ req }: { req: Request }): Promise<Context> => ({
   prisma,
+  req,
 });
