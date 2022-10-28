daf720d5ee99492b9b720039477ba3bf22d31a0b
Ritesh Patil <riteshsp2000@gmail.com>
2022-10-28 22:51:01 +0530

    fix: resolve all issues

--- api/src/rest/controller/instamojo/webhook.ts
+++ api/src/rest/controller/instamojo/webhook.ts
@@ -1,38 +1,40 @@
 /* eslint-disable camelcase */
-import {
-  Request,
-  Response,
-} from 'express';
+import { Request, Response } from 'express';
 import { prisma } from 'utils/context';
 
 export const instaMojowebhook = async (req: Request, res: Response) => {
   const { buyer, payment_id, amount, purpose, status } = req.body;
 
-  if (status === 'Credit') {
-    const user = await prisma.user.update({
-      where: {
-        email: buyer,
-      },
-      data: {
-        festID: {
-          push: 'INNOVISION-2022',
+  try {
+    if (status === 'Credit') {
+      const user = await prisma.user.update({
+        where: {
+          email: buyer,
         },
-      },
-    });
+        data: {
+          festID: {
+            push: 'innovision-2022',
+          },
+        },
+      });
 
-    await prisma.transaction.create({
-      data: {
-        amount,
-        userID: user.id,
-        transactionID: payment_id,
-        type: 'REGISTRATION',
-        timestamp: new Date(),
-        orgID: '635828887ac60f55b4b3c169',
-        comment: purpose,
-      },
-    });
+      await prisma.transaction.create({
+        data: {
+          amount: parseInt(amount, 10),
+          userID: user.id,
+          transactionID: payment_id,
+          type: 'REGISTRATION',
+          timestamp: new Date(),
+          orgID: '635c04868e47acb9dd1ba92d',
+          comment: purpose,
+        },
+      });
 
-    return res.send('ok');
+      return res.status(200).send('ok');
+    }
+
+    return res.send(400).send('Not Credit Type');
+  } catch (error) {
+    return res.status(500).send(error);
   }
-  return res.send('Not Credit Type');
 };
