69880174150f7424eb08ba26371dd4815732d596
Abhibhaw Asthana <39991296+abhibhaw@users.noreply.github.com>
2022-10-30 17:02:30 +0530

    fix: Zimbra API Call failing (#135)

--- api/src/rest/controller/base/zimbra.ts
+++ api/src/rest/controller/base/zimbra.ts
@@ -3,26 +3,23 @@ import { Request, Response } from 'express';
 
 export const zimbraController = async (req: Request, res: Response) => {
   const { username, password } = req.query;
-
+  if (!username || !password)
+    return res.status(400).send('Missing username or password');
   try {
-    const { status } = await axios.get(
-      'https://mail.nitrkl.ac.in/home/~/?auth=ba',
-      {
-        auth: {
-          username: `${username}@nitrkl.ac.in`,
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          password,
-        },
+    await axios.get('https://mail.nitrkl.ac.in/home/~/?auth=sc', {
+      auth: {
+        username: `${username}@nitrkl.ac.in`,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        password,
       },
-    );
-
-    if (status === 401) {
-      res.status(200).send('success');
-    } else {
-      res.status(401).send('invalid credentials');
-    }
+    });
+    return res.status(401).send('invalid credentials');
   } catch (error) {
-    res.status(500).send(error);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const cookie = error?.response.headers['set-cookie'];
+    if (cookie) return res.status(200).send('success');
+    return res.status(401).send('invalid credentials');
   }
 };
