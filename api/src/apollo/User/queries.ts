e802a9750d6975ec007e406756390a6e6b17ebac
Ritesh Patil <riteshsp2000@gmail.com>
2022-11-04 20:16:55 +0530

    fix: add authorization to user queries; create singular user query

--- api/src/apollo/User/queries.ts
+++ api/src/apollo/User/queries.ts
@@ -1,8 +1,10 @@
-import { idArg, inputObjectType, list, nonNull, queryField, stringArg } from 'nexus';
+import { checkGqlPermissions } from 'helpers/auth/checkPermissions';
+import { booleanArg, idArg, inputObjectType, list, nonNull, queryField, stringArg } from 'nexus';
 
 export const getUser = queryField('getUser', {
   type: 'User',
   description: 'Returns a user whose id or email is passed',
+  authorize: (_parent, _args, ctx) => checkGqlPermissions(ctx, []),
   args: {
     id: idArg(),
     uid: idArg(),
@@ -49,6 +51,7 @@ export const GetUsersInputType = inputObjectType({
 export const getUsers = queryField('getUsers', {
   type: list('User'),
   description: 'Returns a list of all the users depending upon the arguments',
+  authorize: (_parent, _args, ctx) => checkGqlPermissions(ctx, []),
   args: {
     params: nonNull('GetUsersInputType'),
   },
@@ -78,6 +81,72 @@ export const getUsers = queryField('getUsers', {
         where: {
           ...args.params,
           festID: undefined,
+        },
+      });
+    }
+
+    return prisma.user.findMany();
+  },
+});
+
+export const user = queryField('user', {
+  type: list('User'),
+  description: 'Returns a list of users depending upon the parameters passed',
+  authorize: (_parent, _args, ctx) => checkGqlPermissions(ctx, []),
+  args: {
+    id: idArg(),
+    uid: idArg(),
+    email: stringArg(),
+    state: stringArg(),
+    city: stringArg(),
+    college: stringArg(),
+    stream: stringArg(),
+    referredBy: stringArg(),
+    festID: list(nonNull(stringArg())),
+    isNitrStudent: booleanArg(),
+  },
+  resolve(_parent, args, { prisma }) {
+    const {
+      id,
+      uid,
+      email,
+      state,
+      city,
+      college,
+      stream,
+      referredBy,
+      festID,
+      isNitrStudent,
+    } = args;
+
+    if (id || uid || email) {
+      return prisma.user.findMany({
+        where: {
+          id: id || undefined,
+          email: email || undefined,
+          uid: uid || undefined,
+        },
+      });
+    }
+
+    if (state || city || college || stream || referredBy || isNitrStudent) {
+      return prisma.user.findMany({
+        where: {
+          state: state || undefined,
+          city: city || undefined,
+          college: college || undefined,
+          stream: stream || undefined,
+          referredBy: referredBy || undefined,
+        },
+      });
+    }
+
+    if (festID) {
+      return prisma.user.findMany({
+        where: {
+          festID: {
+            hasEvery: args.festID || [],
+          },
         },
       });
     }
