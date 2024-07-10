705f47056f39cefe3f0e3f8a47812573a6d1bd63
Srishty M <srishtymangutte@gmail.com>
2024-07-11 03:26:27 +0530

    feat: added query to get all users for the portal for organisers

--- api/src/apollo/User/queries.ts
+++ api/src/apollo/User/queries.ts
@@ -136,3 +136,17 @@ export const getUser = queryField('getUser', {
     });
   },
 });
+
+export const getAllUsers = queryField('getAllUsers', {
+  type: list('User'),
+  description: `Returns a list of users`,
+  authorize: (_parent, _args, ctx) =>
+    checkGqlPermissions(ctx, [
+      PERMISSIONS.SUPER_ADMIN,
+      PERMISSIONS.SUPER_EDITOR,
+      PERMISSIONS.SUPER_VIEWER,
+    ]),
+  resolve: async (_parent, _args, { prisma }) => {
+    return prisma.user.findMany();
+  },
+});
